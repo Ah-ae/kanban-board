31dc40e5e1fa36cde5f3893e147aeed24ca48324
Ah-ae <kimaae@gmail.com>
2024-03-02 20:48:13 +0900

    fix: randomUUID 제거

--- app/atoms/ticketState.ts
+++ app/atoms/ticketState.ts
@@ -26,32 +26,32 @@ export const ticketState = atom<TicketState>({
   default: {
     "TO DO": [
       {
-        id: `ticket-${crypto.randomUUID()}`,
+        id: `ticket-1`,
         title: "Engage Jupiter Express for outer solar system travel",
         description: "",
         label: 1,
         number: 25,
         assignee: {
-          id: `user-${crypto.randomUUID()}`,
+          id: `user-1`,
           name: "John",
           avatar: "3",
         },
       },
       {
-        id: `ticket-${crypto.randomUUID()}`,
+        id: `ticket-2`,
         title: "Create 90 day plans for all departments in the Mars Office",
         description: "",
         label: 2,
         number: 12,
       },
       {
-        id: `ticket-${crypto.randomUUID()}`,
+        id: `ticket-3`,
         title: "Engage Saturn's Rings Resort as a preferred provider",
         description: "",
         label: 1,
         number: 17,
         assignee: {
-          id: `user-${crypto.randomUUID()}`,
+          id: `user-2`,
           name: "Amy",
           avatar: "7",
         },
@@ -59,25 +59,25 @@ export const ticketState = atom<TicketState>({
     ],
     "IN PROGRESS": [
       {
-        id: `ticket-${crypto.randomUUID()}`,
+        id: `ticket-4`,
         title: "Requesting available flights is now taking > 5 seconds",
         description: "",
         label: 3,
         number: 8,
         assignee: {
-          id: `user-${crypto.randomUUID()}`,
+          id: `user-3`,
           name: "Mark",
           avatar: "6",
         },
       },
       {
-        id: `ticket-${crypto.randomUUID()}`,
+        id: `ticket-5`,
         title: "Engage Saturn Shuttle Lines for group tours",
         description: "",
         label: 1,
         number: 15,
         assignee: {
-          id: `user-${crypto.randomUUID()}`,
+          id: `user-4`,
           name: "Timothy",
           avatar: "10",
         },
@@ -85,7 +85,7 @@ export const ticketState = atom<TicketState>({
     ],
     "CODE REVIEW": [
       {
-        id: `ticket-${crypto.randomUUID()}`,
+        id: `ticket-6`,
         title: "Register with the Mars Ministry of Revenue",
         description: "",
         label: 1,
@@ -94,13 +94,13 @@ export const ticketState = atom<TicketState>({
     ],
     DONE: [
       {
-        id: `ticket-${crypto.randomUUID()}`,
+        id: `ticket-7`,
         title: "Homepage footer uses an inline style - should use a class",
         description: "",
         label: 4,
         number: 68,
         assignee: {
-          id: `user-${crypto.randomUUID()}`,
+          id: `user-5`,
           name: "Joyce",
           avatar: "8",
         },
